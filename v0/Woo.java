b1170cacda8cd85ebce940a0984b8d3c42317ada
Ari Gurovich <agurovich20@stuy.edu>
2022-01-13 14:03:22 -0500

    initsetup

--- v0/Woo.java
+++ v0/Woo.java
@@ -1,1 +1,10 @@
+import java.io;
+import java.util;
+import java.util.Scanner;
 
+public class Woo {
+	public static void main(String[] args) {
+		StuyTrail run = new StuyTrail();
+		Scanner in = new Scanner (System.in);
+	}
+}
