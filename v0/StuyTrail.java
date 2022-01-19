b3a01c6962df0fa08ae8c639948ead102f1cc0b6
Marcus Wu <mwu20@stuy.edu>
2022-01-19 14:06:24 -0500

    asjdias

--- v0/StuyTrail.java
+++ v0/StuyTrail.java
@@ -1,4 +1,20 @@
 public class StuyTrail {
-	public String startGame() {
-		return "Welcome to the StuyTrail: have fun getting to Stuyvesant!\nPress enter to continue."
+	public void startGame() {
+		System.out.println( "Welcome to the StuyTrail: have fun getting to Stuyvesant!\nPress enter to continue." );
+		System.out.println( "What is your name?" );
 	}
+/*
+	public String choiceInt(String choice) {
+		if ( choice.equals("1") 
+		   || choice.equals("2")
+		   || choice.equals("3")
+		   || choice.equals("4") )
+			return choice;
+		else {
+			return "Please input an integer (1,2,3,4)";
+			//System.out.println( "Please input an integer (1, 2, 3, 4)" );
+			//choiceInt(choice);
+		}
+*/
+	}
+}
