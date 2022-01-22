1e837b9883b199de598d15a6407874efa0a71d04
Oscar Breen <obreen30@stuy.edu>
2022-01-21 21:29:05 -0500

    hh

---
+++ v5/v6/Woo.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+//import java.util.Scanner;
+
+public class Woo {
+        public static void main(String[] args) {
+                StuyTrail game = new StuyTrail();
+
+                game.startGame();
+
+        }
+}
+
